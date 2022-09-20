ec60c917bc2d4b0a61f49f170a6a222da7af13b6
Octavio Blanco <70592116+OblancC@users.noreply.github.com>
2022-09-20 20:56:55 -0300

    Update main.c
    Codigo atualizado 20/09

--- main.c
+++ main.c
@@ -8,18 +8,18 @@ int main()
 
     float totalfinal,total, quantidade, total_parcelas, valor_recebido, troco,desconto,price;
     int opc,parcelas;
+    do{
+        printf("\t\t\t\t---------- Catalogo Padaria ----------\n\n");
+        printf("\t\t\t\t1 - Pão de forma ------ R$7,50\n");
+        printf("\t\t\t\t2 - Pão de centeio ---- R$8,69\n");
+        printf("\t\t\t\t3 - Broa de milho ----- R$5,00\n");
+        printf("\t\t\t\t4 - Sonho ------------- R$4,50\n");
+        printf("\t\t\t\t5 - Tubaína ----------- R$3,25\n\n");
 
-    printf("\t\t\t\t---------- Catalogo Padaria ----------\n\n");
-    printf("\t\t\t\t1 - Pão de forma ------ R$7,50\n");
-    printf("\t\t\t\t2 - Pão de centeio ---- R$8,69\n");
-    printf("\t\t\t\t3 - Broa de milho ----- R$5,00\n");
-    printf("\t\t\t\t4 - Sonho ------------- R$4,50\n");
-    printf("\t\t\t\t5 - Tubaína ----------- R$3,25\n\n");
-
-    printf("\t\t\t\t------Selecione a opção desejada: ");
-    scanf("%d",&opc); //Switch
-    getchar(); //pra garantir que vai pegar o enter
-    system("cls"); //limpar a tela
+        printf("\t\t\t\t------Selecione a opção desejada: ");
+        scanf("%d",&opc); //Switch e condição do loop
+        getchar(); //pra garantir que vai pegar o enter
+        system("cls"); //limpar a tela
 
     switch(opc){
         case 1:
@@ -48,6 +48,7 @@ int main()
         break;
         default:
             printf("\n\t\tOpção inválida!!! Digite de 1 a 5\n");
+            system("cls");
             return 0; //fechar codigo caso opc errada
     }//Fim do switch case
 
@@ -56,10 +57,14 @@ int main()
     getchar();
     system("cls");
 
-    total=quantidade*price; //Quantidade * valor do item
-
+    total=total+(quantidade*price); //Quantidade * valor do item
     printf("\n\t\tO valor total foi de R$%.2f\n\n",total);
     printf("\t\t--------------------------\n");
+    printf("\t\tDeseja selecionar outro produto?\n\t\t(1) - Sim\n\t\t\(2) - Não\n\t\t");
+    scanf("%d", &opc);
+    getchar();
+    }while(opc==1);
+
 
     printf("\t\t(1) Parcelar \n\t\t(2) A vista\n");
     printf("\n\t\tQual seria a Forma de Pagamento? ");
@@ -76,10 +81,10 @@ int main()
             totalfinal=total*1.05;
             total_parcelas=totalfinal/parcelas;
             }
-        else if (parcelas>3){ //8% Juros
-            totalfinal=total*1.08;
-            total_parcelas=totalfinal/parcelas;
-            }
+            else if (parcelas>3){ //8% Juros
+                totalfinal=total*1.08;
+                total_parcelas=totalfinal/parcelas;
+                }
 
         system("cls");
         printf("\n\t|------------------NOTA-------------------|\n\n");\
@@ -100,13 +105,13 @@ int main()
             troco=valor_recebido-desconto;
             }
             else if(total>50 && total<100){ //10% Desconto
-            desconto=total*0.90;
-               troco=valor_recebido-desconto;
-            }
-            else if(total>=100){ //18% Desconto
-                desconto=total*0.82;
-                troco=valor_recebido-desconto;
-            }
+                    desconto=total*0.90;
+                    troco=valor_recebido-desconto;
+                    }
+                    else if(total>=100){ //18% Desconto
+                        desconto=total*0.82;
+                        troco=valor_recebido-desconto;
+                        }
 
         system("cls");
         printf("\n\t|------------------NOTA-------------------|\n\n");\
@@ -118,6 +123,9 @@ int main()
         if(troco<0){
             printf("\n\t--__-- Ficou faltando R$%.2f --__--\n",troco);
         }
-    }//Fim if A VISTA
+    }       // Fim if A VISTA
+    if(opc != 1 && opc !=2){
+        printf("\t\tSeleção de pagamento invalido. Selecione opções de 1 a 2\n");
+    }    //Fim do if da opc invalida de pagamento.
     return 0;
-    }
+}
