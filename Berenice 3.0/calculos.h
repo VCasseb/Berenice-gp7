3a3b6571fc983ffd3a1be3b212392e892c76f37d
Vinicius Casseb <vinicasseb99@gmail.com>
2022-10-27 12:23:30 -0300

    ok

--- Berenice 3.0/calculos.h
+++ Berenice 3.0/calculos.h
@@ -5,17 +5,17 @@ extern int opc,sub_opc;
 
 //----------Funcoes para o menu iniciar
 void mn_inicial(){
-
+    system("cls");
     printf("\n\t\t\t(1) - Produtos");
     printf("\n\t\t\t(2) - Vendas");
     printf("\n\t\t\t(3) - Sair");
     printf("\n\t\t\t---> ");
     scanf("%d",&opc);
-
+    system("cls");
     return;
 }
 
-void switch_menu(){
+void switch_menu(){ //Swithc para o menu iniciar
 
     switch(opc){
 
@@ -33,8 +33,9 @@ void switch_menu(){
 
 void sub_produtos(){
 
-    printf("\n\t\t\tVoce selecionou Produtos");
+    printf("\n\t\t\tVoce selecionou Produtos\n");
     do{
+
         printf("\n\t\t\t(1) - Exibir");
         printf("\n\t\t\t(2) - Cadastrar");
         printf("\n\t\t\t(3) - Atualizar");
@@ -44,7 +45,7 @@ void sub_produtos(){
         printf("\n\t\t\t(7) - Voltar");
         printf("\n\t\t\t---> ");\
         scanf("%d",&sub_opc);
-
+        system("cls");
         if(sub_opc <1 || sub_opc >7){
             printf("\n\t\t\tOpcao Invalida!");
         }
@@ -55,7 +56,7 @@ void sub_produtos(){
 
 void sub_vendas(){
 
-    printf("\n\t\t\tVoce selecionou Vendas");
+    printf("\n\t\t\tVoce selecionou Vendas\n");
 
     do{
         printf("\n\t\t\t(1) - Realizar Venda");
@@ -63,7 +64,7 @@ void sub_vendas(){
         printf("\n\t\t\t(3) - Voltar");
         printf("\n\t\t\t---> ");\
         scanf("%d",&sub_opc);
-
+        system("cls");
         if(sub_opc <1 || sub_opc >3){
             printf("\n\t\t\tOpcao Invalida!");
         }
