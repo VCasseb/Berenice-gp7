5de3fc15fbd8628bef79b0af06f92cadbfa24661
Vinicius Casseb <vinicasseb99@gmail.com>
2022-11-08 14:18:12 -0300

    indentacao

--- Atualizei_vini/main.c
+++ Atualizei_vini/main.c
@@ -21,108 +21,118 @@ int main(int argc, char *argv[])
         printf("Erro na alocação de memória");
         return(1);
     }
-    do{// do menu principal
-    mn_inicial();
-    scanf("%d",&opc);
-    if(opc == 1){
-    do{// do sub menu
-        limpar_tela();
-        sub_protudos();
-        scanf("%d", &opc);
-        getchar();
-        limpar_tela();
-        switch(opc)
+    do // do menu principal
+    {
+        mn_inicial();
+        scanf("%d",&opc);
+        if(opc == 1)
         {
-            case 0:
-                break;
-
-            //Opção de Exibir os Produtos já Cadastrados
-            case 1:
-                if(tam == 0)
+            do // do sub menu
+            {
+                limpar_tela();
+                sub_protudos();
+                scanf("%d", &opc);
+                getchar();
+                limpar_tela();
+                switch(opc)
                 {
-                    printf("...Vazio...");
-                }
-                else
-                {
-                    visualizar_estoque(base, tam);
-                }
-                printf("\n");
-                system("pause");
-                break;
+                case 0:
+                    break;
 
-            //Cadastrar Produtos Novos.
-            case 2:
-                /*aumenta a zona de memória do vetor vet*/
-                base = (item *)realloc(base, (tam+1)*sizeof(item));
-                if(base == NULL)
-                {
-                    printf("ERRO na realocação de memória");
-                    exit(2);
-                }
-                tam = cadastrar_produto(base, tam);
-                break;
+                //Opção de Exibir os Produtos já Cadastrados
+                case 1:
+                    if(tam == 0)
+                    {
+                        printf("...Vazio...");
+                    }
+                    else
+                    {
+                        visualizar_estoque(base, tam);
+                    }
+                    printf("\n");
+                    system("pause");
+                    break;
 
-            //Atualizar um produto ja cadastrado.
-            case 3:
-                if(tam == 0)
-                {
-                    printf("...Vazio...");
-                }
-                else
-                {
-                    atualizar_produto(base, tam);
-                }
-                printf("\n");
-                system("pause");
-                break;
+                //Cadastrar Produtos Novos.
+                case 2:
+                    /*aumenta a zona de memória do vetor vet*/
+                    base = (item *)realloc(base, (tam+1)*sizeof(item));
+                    if(base == NULL)
+                    {
+                        printf("ERRO na realocação de memória");
+                        exit(2);
+                    }
+                    tam = cadastrar_produto(base, tam);
+                    break;
 
-            //Excluir um item ja cadastrado.
-            case 4:
-                if(tam == 0)
-                {
-                    printf("...Vazio...");
+                //Atualizar um produto ja cadastrado.
+                case 3:
+                    if(tam == 0)
+                    {
+                        printf("...Vazio...");
+                    }
+                    else
+                    {
+                        atualizar_produto(base, tam);
+                    }
+                    printf("\n");
+                    system("pause");
+                    break;
+
+                //Excluir um item ja cadastrado.
+                case 4:
+                    if(tam == 0)
+                    {
+                        printf("...Vazio...");
+                    }
+                    else
+                    {
+                        tam = excluir_produto(base, tam);
+                        base = (item *)realloc(base, (tam)*sizeof(item));
+                    }
+                    printf("\n");
+                    system("pause");
+                    break;
+
+                //Salvar os produtos Cadastrados
+                case 5:
+                    fclose(arquivo);
+                    break;
+
+                //Lê a lista de produtos no ultimo arquivo salvo.
+                case 6:
+
+                    if((arquivo == fopen(argv[1], "r"))==NULL)
+                    {
+                        printf("Erro na abertura do Arquivo.");
+                    }
+                    else
+                    {
+                        limpar_tela();
+                        printf("Arquivo Aberto com sucesso.");
+                    }
+
+                default:
+                    continue;
+
                 }
-                else
-                {
-                    tam = excluir_produto(base, tam);
-                    base = (item *)realloc(base, (tam)*sizeof(item));
-                }
-                printf("\n");
-                system("pause");
-                break;
-
-            //Salvar os produtos Cadastrados
-            case 5:
-                fclose(arquivo);
-                break;
-
-            //Lê a lista de produtos no ultimo arquivo salvo.
-            case 6:
-
-                if((arquivo == fopen(argv[1], "r"))==NULL){
-                    printf("Erro na abertura do Arquivo.");
-                }else{
-                    limpar_tela();
-                    printf("Arquivo Aberto com sucesso.");
-                }
-
-            default:
-                continue;
+            }
+            while(opc != 0);
 
         }
-    }while(opc != 0);
+        if(opc == 2)
+        {
+            limpar_tela();
+            if(tam<=0)
+            {
+                printf("...Vazio...");
+                exit(1);
+            }
+            realizar_venda(base,rel,tam);
 
-    }
-    if(opc == 2){
-        limpar_tela();
-        if(tam<=0){
-            printf("...Vazio...");
-            exit(1);
         }
-        realizar_venda(base,rel,tam);
-
     }
-    }while(opc !=3);
+    while(opc !=3);
     free(base);
     return 0;
 
