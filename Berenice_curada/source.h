389308d8189a05597c5cf695be64ec4411e9b35b
Octavio Blanco <70592116+OblancC@users.noreply.github.com>
2022-11-09 19:46:04 -0300

    Add files via upload

---
+++ Berenice_curada/source.h
@@ -0,0 +1,27 @@
+#ifndef SOURCE_H_INCLUDED
+#define SOURCE_H_INCLUDED
+
+//Typedef ele redefine um tipo, ele da um apelido para o tipo de dados ja existente
+typedef struct estrutura //struct principal
+{
+    int codigo;
+    char nome[26];
+    float valor_unitario;
+    int estoque;
+} item;
+
+void mn_inicial(); // Printf do menu linha 23
+void sub_protudos(); //Printf do submenu linha 32
+void limpar_tela(); //SystemCLS linha 45
+void visualizar_estoque (item *base, int tam); //linha 50
+int cadastrar_produto(item *base, int tam, int quantidade);
+void atualizar_produto (item *base, int tam);
+int excluir_produto(item *base, int tam);
+void reordena_estoque(item *base, int tam); //buble sort
+void salvar(item *base, int tam); //Salver cupom fiscal
+void ler(item *base, int tam);
+void sub_vendas();
+void relatorio(item *base, int tam,int qtd_vendas);
+void realizar_venda(item *base, int tam,int qtd_vendas);
+
+#endif // SOURCE_H_INCLUDED
